67cb993280cf7a6ac96f6f756ae837f6ee941884
Raiana Sousa dos Santos <53270687+raianasousa@users.noreply.github.com>
2020-03-26 18:14:08 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,39 +1,46 @@
 import java.io.*;
 
-public class Menu {
+class Main {
+    private static Object IOException;
 
-        public static  void menu() throws IOException { // menu principal
-            DataInputStream io = new DataInputStream(System.in);
-            int opcao =0;
-            do {
-                System.out.println("\n\n - Menu Restaurante -\n\n");
-                System.out.println("\n   =========================");
-                System.out.println("   |     1 - Nossos pratos     |");
-                System.out.println("   |     2 - Pagar             |");
-                System.out.println("   |     0 - Sair              |");
-                System.out.println("    =========================\n");
-                System.out.print("Digite a opção: ");
+    public Main() throws IOException {
+        DataInputStream io = new DataInputStream(System.in);
+        int opcao = 0;
+        do {
+            System.out.println("\n\n - Menu Restaurante -\n\n");
+            System.out.println("\n   =========================");
+            System.out.println("   |     1 - Nossos pratos     |");
+            System.out.println("   |     2 - Pagar             |");
+            System.out.println("   |     0 - Sair              |");
+            System.out.println("    =========================\n");
+            System.out.print("Digite a opção: ");
 
-                opcao = io.readInt();
-                System.out.print("\n");
-                switch (opcao) {
-                    case 1:
-                        System.out.println(new NossosPratos());
-                        System.out.println();
-                        return;
-                    case 2:
-                        System.out.println(new ValorTotal());
-                        return;
-                    case 0:
-                        break;
+            opcao = io.readInt();
+            System.out.print("\n");
+            switch (opcao) {
+                case 1:
+                    System.out.println(new NossosPratos());
+                    System.out.println();
+                    return;
+                case 2:
+                    System.out.println(new ValorTotal());
+                    return;
+                case 0:
+                    break;
 
-                    default:
-                        return ;
-                }
-            } while (opcao != 0);
-
-
+                default:
+                    return;
+            }
+        } while (opcao != 0);
 
 
-        }
+    }
+
+
+
+    private static void Main() {
+    }
+
+    private void menu() {
+    }
 }
